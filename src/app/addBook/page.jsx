6c79b5eaa4e526947520ee9ed5410fa259c8d430
Sucharita <sucharitasardar7@gmail.com>
2026-01-18 17:11:56 +0530

    Delete src directory

--- src/app/addBook/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div>this is add project page</div>;
-};
-
-export default page;
--- src/app/book/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div>total book pages</div>;
-};
-
-export default page;
